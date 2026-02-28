e6a692d889b9fe599e24007b0fedda5f566a3b18
Johan Björkgren <jobj0046@student.hv.se>
2026-02-28 17:02:13 +0100

    Remove hello

--- RiderGithubDemo/Program.cs
+++ RiderGithubDemo/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Github!");
+        Console.WriteLine("Github!");
         Console.WriteLine("A");
         Console.WriteLine("B");
     }
